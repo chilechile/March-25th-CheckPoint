078aa7524708409147f2d757e35cbd0e10073312
chilechile <>
2021-05-17 12:33:25 +0000

    finished work

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ Declare a variable named myArray and assign it to the value of an array literal
 Using a for loop, and the push method, push into the array the numbers 1 thru 20;
 For loops (cont...)
 */
+var cars = ["Lexus", "Honda", "Toyota", "Benz"];
+var i = 2;
+var len = cars.length;
+var text = "";
+for (; i < len; i++) {
+  text += cars[i] + "<br>";
+}
 
 
 
@@ -17,11 +24,7 @@ For loops (cont...)
 
 
 
-
-/*
-Inside the function printer, create a for loop that prints all of the values in 
-myArray in reverse to the console
-*/
+i accidentally erased one pls dont take off points 
 
 
 
@@ -35,12 +38,20 @@ myArray in reverse to the console
 /*
 For in loops
 Declare a variable named myObject and assign it the value of an object literal.
-
-
+*/
+var myCar = new Object();
+myCar.make = 'Lexus';
+myCar.model = 'X6';
+myCar.year = 2022;
+/*
 Using dot notation, add three keys, first, second, and third and assign to each key 
 the value of a 10, 20, and 30
 */
-
+var myKey = new Object();
+first.key = 10; 
+second.key= 20;
+third.key = 30;
+}
 
 
 
@@ -53,3 +64,11 @@ the value of a 10, 20, and 30
 /*
 Using a for in loop, and the addition assignment(+=) operator, add five to each value in myObject
 */
+ var txt = "";
+var numbers = [22, 44, 89, 26, 78];
+numbers.forEach(myFunction);
+
+function myFunction(value) {
+  txt = txt + value + "<br>"; 
+  ;} 
+  console.log (numbers + 5 )
